75568b060ca1fbd6f8bda41555e36397b70a1287
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-05 01:09:56 +0600

    Install React Icon

--- src/component/HomePage/All Books/Book/Book.jsx
+++ src/component/HomePage/All Books/Book/Book.jsx
@@ -3,7 +3,26 @@
 const Book = ({book}) => {
     return (
       <div>
-        <h1>Name: {book.bookName}</h1>
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <figure>
+            <img src={book.image} alt={book.bookName} />
+          </figure>
+          <div className="flex gap-4 mt-2">
+            {book.tags.map((tag) => (
+              <div className="badge badge-soft badge-success font-bold">
+                {tag}
+              </div>
+            ))}
+          </div>
+          <div className="card-body">
+            <h2 className="card-title text-2xl">{book.bookName}</h2>
+            <p className="font-semibold text-gray-400">By: {book.author}</p>
+            <div className="card-actions border-t border-dashed border-gray-300 pt-4 flex justify-between">
+              <div className="text-xl font-semibold">{book.category}</div>
+              <div className="text-xl font-semibold">{book.rating}</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
 };
